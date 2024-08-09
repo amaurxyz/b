d183997f3b7ec35f722c58162dc231c2fd269630
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2024-08-09 09:17:58 -0300

    controllers.py

--- mvc_flask/mvc_flask/__pycache__/templates/controllers.py
+++ mvc_flask/mvc_flask/__pycache__/templates/controllers.py
@@ -6,3 +6,9 @@ tarefa_controller = Blueprint("tarega", __name__)
 @tarefa_controller.route("/")
 def index():
     return render_template("index.html", tarefas=tarefas)
+
+@tarefa_controller.route('/add', methods=['POST'])
+def add():
+    descricao = request.form['descricao']
+    add_tarefa(descricao)
+    return redirect(url_for('tarefa.index'))
